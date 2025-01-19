6a521009f90126012352a353fbcfa905db0aa170
Brendan Ashby <brendan@brendanashby.com>
2025-01-18 17:41:52 -0800

    update readme and printouts

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 A little repo for helping Anna :)
+
+###
+
+To run:
+
+- `npm install`
+- make a `.env` file with `OAUTH_KEY=your_auth_key`
+- then run `bun run .\anna_test_api.ts` (or w/e you use to run ts files)
--- anna_test_api.ts
+++ anna_test_api.ts
@@ -5,5 +5,9 @@ dotenv.config();
 fsqDevelopers.auth(process.env.OAUTH_KEY as string);
 fsqDevelopers
   .getUserCheckins({ v: "20231010", limit: "100", offset: "0" })
-  .then(({ data }) => console.log(data))
+  .then(({ data }) => {
+    console.log(data);
+    console.log("\n\nOk, here is the first checkin:");
+    console.log(data.response.checkins);
+  })
   .catch((err) => console.error(err));
